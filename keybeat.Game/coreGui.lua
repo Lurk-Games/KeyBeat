ac15c589ad4233e237212019b49a56dc1847012b
VirusAnimates <103518350+Virus01Official@users.noreply.github.com>
2023-11-25 10:28:28 +0100

    Delete keybeat.Game/coreGui.lua

--- keybeat.Game/coreGui.lua
+++
@@ -1,5 +0,0 @@
-local StarterGui = game:GetService('StarterGui')
-local UIS = game:GetService('UserInputService')
-
-StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.All, false)
-StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList, true)
