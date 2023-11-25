7b6a1eaea7f3f4814de3c3caeba77288535392a3
VirusAnimates <103518350+Virus01Official@users.noreply.github.com>
2023-11-25 10:28:04 +0100

    Create coreGui.lua

---
+++ keybeat.Game/coreGui.lua
@@ -0,0 +1,5 @@
+local StarterGui = game:GetService('StarterGui')
+local UIS = game:GetService('UserInputService')
+
+StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.All, false)
+StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList, true)
