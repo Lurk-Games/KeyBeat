45dcace2cf9ba422fae39cd411cf691a02e2da13
VirusAnimates <103518350+Virus01Official@users.noreply.github.com>
2023-11-27 08:33:44 +0100

    Create tutorial.lua

---
+++ keybeat.song/tutorial.lua
@@ -0,0 +1,30 @@
+local module = {}
+module.id = 14403997163
+module.offset = 500
+module.thumbnail = "rbxassetid://2762577836"
+module.credits = "By:Lurk Games"
+module.mapper = "Mapped by:Lurk Games"
+module.difficulty = "Difficulty: Easy"
+module.category = "Original"
+module.scrollspeed = 1
+
+module.notes = {
+	{321,3};
+	{750,2};
+	{1285,3};
+	{1714,2};
+	{2250,4};
+	{2571,1};
+	{3214,4};
+	{3535,1};
+	{4071,3};
+	{4392,2};
+	{4928,3};
+	{5250,2};
+	{6000,4};
+	{6214,1};
+	{6857,3};
+	{7071,2};
+}
+
+return module
