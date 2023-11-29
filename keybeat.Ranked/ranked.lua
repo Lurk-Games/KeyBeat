8ee3999ce23b5aaa1774ef82b027dc79b73c25b5
VirusAnimates <103518350+Virus01Official@users.noreply.github.com>
2023-11-29 06:19:20 +0100

    Create ranked.lua

---
+++ keybeat.Ranked/ranked.lua
@@ -0,0 +1,3 @@
+--HAHAHAHA YOU THOUGHT
+
+--To get the map ranked we need to accept the map and then your map will be as the main game beatmap(you will also receive a rank for getting your map ranked)
